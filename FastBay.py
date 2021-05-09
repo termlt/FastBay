545684ce97a2e89e2fced8fcca4dcaf9f740e4e8
termlt <75373156+termlt@users.noreply.github.com>
2021-05-09 17:27:45 +0400

    Add files via upload

---
+++ FastBay.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+#________________________________________________
+
+print('Script is written by: termit ( termlt - on GitHub ) \n')
+
+search = input('Search: ')
+
+r = requests.get(f'https://thepiratebay10.org/search/{search}')
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+try:
+	title = soup.find('div', class_ = 'detName')
+	links = soup.find('td', class_ = None)
+	link = links.find('a', class_ = None)
+
+	print()
+	print('Title: ' + title.find('a')['title'].strip('Details for'))
+	print()
+	print('Link for download: ' + link['href'])
+
+except:
+	print(f'No torrent was found for {search}.')	
