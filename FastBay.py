fa968f33003b5c2b278ae6e56b93a2694668c1ec
termlt <75373156+termlt@users.noreply.github.com>
2021-05-14 09:50:08 -0700

    Update FastBay.py
    Added Information about the torrent + some grammar fixes.

--- FastBay.py
+++ FastBay.py
@@ -1,10 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-#________________________________________________
-
-print('Script is written by: termit ( termlt - on GitHub ) \n')
-
 search = input('Search: ')
 
 r = requests.get(f'https://thepiratebay10.org/search/{search}')
@@ -12,12 +8,35 @@ r = requests.get(f'https://thepiratebay10.org/search/{search}')
 soup = BeautifulSoup(r.content, 'html.parser')
 
 try:
+	#Title
 	title = soup.find('div', class_ = 'detName')
-	links = soup.find('td', class_ = None)
-	link = links.find('a', class_ = None)
+
+	#Link for the download
+
+	#Getting the class where the <a href> is 
+	link_main_class = soup.find('td', class_ = None)
+	#Finding <a href>
+	link = link_main_class.find('a', class_ = None)
+
+	#Info about the torrent file
+
+	#Getting the class where the information about the file is 
+	information_class = soup.find('font', class_ = 'detDesc')
+	#Getting the text from information_class
+	get_text = information_class.get_text()
+	#Removing word Uploaded from the text
+	strip_uploaded = get_text.strip('Uploaded')
+	#Replace , ULed by   with   Uploader:
+	replace_uled = strip_uploaded.replace(', ULed by', ',  Uploader:')
+
+	information = replace_uled.replace('Size', ' Size: ')
+		 
 
 	print('\nTitle: ' + title.find('a')['title'].strip('Details for'))
-	print('\nLink for download: ' + link['href'])
+
+	print(f'\nInformation about the torrent \nUploaded: {information}')
+
+	print('\nLink for a download: ' + link['href'])
 
 except:
-	print(f'No torrent was found for {search}.')	
+	print(f'\nNo torrent was found for {search}.')
