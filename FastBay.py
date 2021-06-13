2cd3bc0e66807f6513e8c6036c303d128d162b17
termlt <75373156+termlt@users.noreply.github.com>
2021-06-13 19:20:18 +0400

    Update FastBay.py

--- FastBay.py
+++ FastBay.py
@@ -1,41 +1,58 @@
+'''
+NOTE: YOU NEED TO PROVIDE THE WHOLE NAME OF A TORRENT.
+WRONG: charlie and the choco
+RIGHT: charlie and the chocolate factory
+'''
+
+
 import requests
 from bs4 import BeautifulSoup
 
-search = input('Search: ')
+class fb:
 
-r = requests.get(f'https://thepiratebay10.org/search/{search}')
-soup = BeautifulSoup(r.content, 'html.parser')
+	def __init__(self, search):
+		self.search = search
 
-try:
-	#Title
-	title = soup.find('div', class_ = 'detName')
+	def main(self):
+		r = requests.get(f'https://thepiratebay10.org/search/{self.search}')
 
-	#Link for the download
+		soup = BeautifulSoup(r.content, 'html.parser')
 
-	#Getting the class where the <a href> is 
-	link_main_class = soup.find('td', class_ = None)
-	#Finding <a href>
-	link = link_main_class.find('a', class_ = None)
+		try:
+			#Title
+			title = soup.find('div', class_ = 'detName')
 
-	#Info about the torrent file
+			#Link for the download
 
-	#Getting the class where the information about the file is 
-	information_class = soup.find('font', class_ = 'detDesc')
-	#Getting the text from information_class
-	get_text = information_class.get_text()
-	#Removing word Uploaded from the text
-	strip_uploaded = get_text.strip('Uploaded')
-	#Replace , ULed by   with   Uploader:
-	replace_uled = strip_uploaded.replace(', ULed by', ',  Uploader:')
+			#Getting the class where the <a href> is
+			link_main_class = soup.find('td', class_ = None)
+			#Finding <a href>
+			link = link_main_class.find('a', class_ = None)
 
-	information = replace_uled.replace('Size', ' Size: ')
+			#Info about the torrent file
+
+			#Getting the class where the information about the file is
+			information_class = soup.find('font', class_ = 'detDesc')
+			#Getting the text from information_class
+			get_text = information_class.get_text()
+			#Removing word Uploaded from the text
+			strip_uploaded = get_text.strip('Uploaded')
+			#Replace , ULed by   with   Uploader:
+			replace_uled = strip_uploaded.replace(', ULed by', ',  Uploader:')
+
+			information = replace_uled.replace('Size', ' Size: ')
+
+			#Category
+			#category = soup.find('center')
 
 
-	print('\nTitle: ' + title.find('a')['title'].strip('Details for'))
+			print('\nTitle: ' + title.find('a')['title'].strip('Details for'))
 
-	print(f'\nInformation about the torrent \nUploaded: {information}')
+			print(f'\nInformation about the torrent \nUploaded: {information}')
 
-	print('\nLink for a download: ' + link['href'])
+			#print('\nCategory: ' + details.get_text())
 
-except:
-	print(f'\nNo torrent was found for {search}.')
+			print('\nLink for a download: ' + link['href'])
+
+		except:
+			print(f'\nNo torrent was found for {self.search}')
