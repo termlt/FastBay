f12ed1c627dff6baee5c6d6f21a215e600f53319
termlt <75373156+termlt@users.noreply.github.com>
2021-06-16 14:36:37 +0400

    Update FastBay.py

--- main/FastBay.py
+++ main/FastBay.py
@@ -8,51 +8,53 @@ RIGHT: charlie and the chocolate factory
 import requests
 from bs4 import BeautifulSoup
 
-class fb:
+def fb(search):
 
-	def __init__(self, search):
-		self.search = search
+    if not search:
+        return '\nSearch string can not be blank!'
 
-	def main(self):
-		r = requests.get(f'https://thepiratebay10.org/search/{self.search}')
+    else:
 
-		soup = BeautifulSoup(r.content, 'html.parser')
+    	r = requests.get(f'https://thepiratebay10.org/search/{search}')
 
-		try:
-			#Title
-			title = soup.find('div', class_ = 'detName')
+    	soup = BeautifulSoup(r.content, 'html.parser')
 
-			#Link for the download
-
-			#Getting the class where the <a href> is
-			link_main_class = soup.find('td', class_ = None)
-			#Finding <a href>
-			link = link_main_class.find('a', class_ = None)
-
-			#Info about the torrent file
-
-			#Getting the class where the information about the file is
-			information_class = soup.find('font', class_ = 'detDesc')
-			#Getting the text from information_class
-			get_text = information_class.get_text()
-			#Removing word Uploaded from the text
-			strip_uploaded = get_text.strip('Uploaded')
-			#Replace , ULed by   with   Uploader:
-			replace_uled = strip_uploaded.replace(', ULed by', ',  Uploader:')
-
-			information = replace_uled.replace('Size', ' Size: ')
-
-			#Category
-			#category = soup.find('center')
+    	try:
+    		#Title
+    		title = soup.find('div', class_ = 'detName')
 
 
-			print('\nTitle: ' + title.find('a')['title'].strip('Details for'))
+    		#Link for the download
 
-			print(f'\nInformation about the torrent \nUploaded: {information}')
+    		#Getting the class where the <a href> is
+    		link_main_class = soup.find('td', class_ = None)
 
-			#print('\nCategory: ' + details.get_text())
+    		#Finding <a href>
+    		link = link_main_class.find('a', class_ = None)
 
-			print('\nLink for a download: ' + link['href'])
 
-		except:
-			print(f'\nNo torrent was found for {self.search}')
+    		#Info about the torrent file
+
+    		#Getting the class where the information about the file is
+    		information_class = soup.find('font', class_ = 'detDesc')
+
+    		#Getting the text from information_class
+    		get_text = information_class.get_text()
+
+    		#Removing word Uploaded from the text
+    		strip_uploaded = get_text.strip('Uploaded')
+
+    		#Replace , ULed by   with   Uploader:
+    		replace_uled = strip_uploaded.replace(', ULed by', ',  Uploader:')
+
+            #Final result
+    		information = replace_uled.replace('Size', ' Size: ')
+
+
+    		return f'''\nTitle: {title.find('a')['title'].strip('Details for')}
+            \nInformation about the torrent \nUploaded: {information}
+    		\nLink for a download:  + {link['href']}'''
+
+
+    	except:
+    		return f'\nNo torrent was found for {search}'
