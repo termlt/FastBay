6e09300ea87e587c776cd6d39cc225b18f76a7a3
termit <75373156+termlt@users.noreply.github.com>
2021-12-22 23:44:17 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Fast torrent parser from PirateBay.'
+LONG_DESCRIPTION = "FastBay allows it's users to find the top result in PirateBay search and get it's link for a download."
+
+# Setting up
+setup(
+    name="fastbay",
+    version=VERSION,
+    author="termit (Arsen Anoyan)",
+    author_email="<anoyan2014@yandex.ru>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['beautifoulsoup4', 'requests'],
+    keywords=['python', 'piratebay', 'search', 'parser', 'parsing', 'information'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
