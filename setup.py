e35b1eeee4a4a818c8277b520f8fa4eb7476ba61
termit <75373156+termlt@users.noreply.github.com>
2021-12-23 00:02:01 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 DESCRIPTION = 'Fast torrent parser from PirateBay.'
-LONG_DESCRIPTION = "FastBay allows it's users to find the top result in PirateBay search and get it's link for a download."
+LONG_DESCRIPTION = file: docs.md
 
 # Setting up
 setup(
     name="fastbay",
-    version = '1.0'
+    version='90980908989',
     author="termit (Arsen Anoyan)",
     author_email="<anoyan2014@yandex.ru>",
+    url="https://github.com/termlt/FastBay",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
@@ -16,7 +17,6 @@ setup(
     install_requires=['bs4', 'requests'],
     keywords=['python', 'piratebay', 'search', 'parser', 'parsing', 'information'],
     classifiers=[
-        "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
@@ -24,4 +24,3 @@ setup(
         "Operating System :: Microsoft :: Windows",
     ]
 )
-
